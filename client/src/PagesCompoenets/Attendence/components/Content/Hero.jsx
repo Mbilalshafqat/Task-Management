c59225c2cdb8f2e54af181c6e17604c3e5b44400
umar793 <125833832+Mbilalshafqat@users.noreply.github.com>
2023-11-21 19:29:25 +0500

    new changes

--- client/src/PagesCompoenets/Attendence/components/Content/Hero.jsx
+++ client/src/PagesCompoenets/Attendence/components/Content/Hero.jsx
@@ -1,11 +1,13 @@
 import { color1 } from "@/compoenets/Colors/Color";
 import React from "react";
+import heroimg from "../../../../Assests/logo.png";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <div className="my-[30px] px-[30px]  w-full flex justify-center place-items-center gap-[20px]">
       <div
-        className="border-[1px] border-[gray] rounded-[4px] p-3 w-[500px] h-[200px]"
+        className="border-[1px] border-[gray] rounded-[4px] p-3 w-[500px] h-[200px] relative"
         style={{ backgroundColor: color1 }}
       >
         <h2 className="text-[27px] mb-2">Hello Muhammad</h2>
@@ -13,6 +15,13 @@ const Hero = () => {
           {" "}
           Here your dashboard for today.
         </p>
+        <Image
+          src={heroimg}
+          width={400}
+          height={200}
+          alt=""
+          className="absolute bottom-[-15px]  right-0"
+        />
       </div>
       <div
         className="border-[1px] border-[gray] rounded-[4px] p-3 w-[500px] h-[200px]"
