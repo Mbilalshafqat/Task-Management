9d3baa813350b05e72d06de738a4f387a573b57a
umar793 <125833832+Mbilalshafqat@users.noreply.github.com>
2023-11-24 19:41:14 +0500

    add color schema for the chart

--- client/src/compoenets/Colors/Color.js
+++ client/src/compoenets/Colors/Color.js
@@ -1,5 +1,5 @@
 export const color1 = "#212022";
 export const color2 = "black";
 export const color3 = "#E92129";
-export const chartColor1 = "#E92129";
+export const chartColor1 = "#E7272A";
 export const chartColor2 = "#51DB2B";
